ef37387aea850aece9ed2b9dd92bb01684497463
Bruno Tarnowski <73197259+bhthoughts@users.noreply.github.com>
2024-07-05 20:10:07 -0300

    Update tbs_blocksize.sql
    added the type description

--- tbs_blocksize.sql
+++ tbs_blocksize.sql
@@ -6,10 +6,12 @@ SET PAGES 200 LIN 200
 SET COLSEP '|'
 
 col tablespace_name for a30
-col block_size for 99999999
+col blksize for a10
+col type for a10
 
 SELECT
     tablespace_name,
-    block_size
+    block_size / 1024 || 'k'                             blksize,
+    decode(bigfile, 'YES', 'bigfile', 'NO', 'smallfile') type
 FROM
     dba_tablespaces;
