4937d97c209fbaa40030c1984e8dacbc3f9b483f
Bruno Tarnowski <73197259+bhthoughts@users.noreply.github.com>
2024-07-18 16:32:22 -0300

    Fixed some bugs

--- Data Guard/dg_check_apply_lag.sql
+++ Data Guard/dg_check_apply_lag.sql
@@ -12,41 +12,44 @@ SELECT
     round((arch.first_time - appl.first_time) * 24 * 60) gap_minutes
 -- , ARCH.THREAD# "Thread", ARCH.SEQUENCE# "Last in Sequence", APPL.SEQUENCE# "Last Applied Sequence", (ARCH.SEQUENCE# - APPL.SEQUENCE#) "Difference"
 FROM
+    -- Last sequence archived in Primary Database
     (
-        SELECT DISTINCT
+        SELECT
+            MAX(first_time) first_time,
             thread#,
-            sequence#,
-            first_time
+            dest_id
         FROM
-            v$archived_log
+            v$archived_log a
         WHERE
-            ( thread#, first_time ) IN (
+            dest_id = (
                 SELECT
-                    thread#, MAX(first_time)
+                    dest_id
                 FROM
-                    v$archived_log
-                GROUP BY
-                    thread#
+                    v$archive_dest
+                WHERE
+                        target = 'STANDBY'
+                    AND status = 'VALID'
             )
+        GROUP BY
+            thread#,
+            dest_id
     ) arch,
+    -- Last sequence applied in Standby
     (
-        SELECT DISTINCT
+        SELECT
+            MAX(first_time) first_time,
             thread#,
-            sequence#,
-            first_time
+            dest_id
         FROM
-            v$log_history
+            v$archived_log a
         WHERE
-            ( thread#, first_time ) IN (
-                SELECT
-                    thread#, MAX(first_time)
-                FROM
-                    v$log_history
-                GROUP BY
-                    thread#
-            )
+            applied = 'YES'
+        GROUP BY
+            thread#,
+            dest_id
     ) appl
 WHERE
-    arch.thread# = appl.thread#
+        arch.thread# = appl.thread#
+    AND arch.dest_id = appl.dest_id
 ORDER BY
     1;
